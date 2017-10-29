5ecca5d2026c57a7833596151b9d5dd2442ff06f
Boris Rybalkin <ribalkin@gmail.com>
2017-10-29 21:16:29 +0000

    cleanup

--- install-sam.sh
+++ install-sam.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ "$#" -lt 2 ]; then
     echo "usage $0 version release"
     exit 1
@@ -11,6 +10,6 @@ VERSION=$1
 RELEASE=$2
 
 SAM=sam-${VERSION}-${ARCH}.tar.gz
-wget http://apps.syncloud.org/apps/${SAM} --progress=dot:giga
-tar xzf $SAM -C /opt/app
+wget http://apps.syncloud.org/apps/${SAM} --progress=dot:giga -O /tmp/$SAM
+tar xzf /tmp/$SAM -C /opt/app
 /opt/app/sam/bin/sam update --release ${RELEASE}
